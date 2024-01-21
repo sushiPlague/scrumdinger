0001e3a7b9e17f893e2c383a7c1daa77e88edcb6
Nikola Lausev <lausevnikola@gmail.com>
2024-01-21 13:12:51 +0100

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Nikola Lausev on 21.1.24..
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
